a987804a19f9508677dfcfbe0ebb9a554a8907f3
Gimena Motto <gimenapimba@gmail.com>
2024-01-12 23:15:48 +0100

    add projects

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,52 +1,162 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
-import img1 from './images/project1.jpg';
-import './Cards.css'
+import { Card, Button, Badge } from 'react-bootstrap';
+import videorater from './videos/rater.mp4';
+import videorick from './videos/rickandmorty.mp4';
+import video360 from './videos/360.mp4';
+import './Cards.css';
 
 const Cards = () => {
-
     const cardStyle = {
         backgroundColor: '#1a1a1a',
         color: '#ffffff',
         border: '1px solid #333333',
     };
+
+    const badgeStyle = {
+        backgroundColor: '#D3E061',
+    };
+
     return (
         <div>
             <Card className="mb-3 mt-5 mx-auto" style={{ ...cardStyle, width: '40rem' }}>
-                <Card.Img variant="top" src={img1} className="p-3" style={{ objectFit: 'contain', maxHeight: '15rem' }} />
+                <h5 className="text-center mb-1 mt-3">Juego 1 poner nombre</h5>
+                <video autoPlay loop className="p-3" style={{ maxHeight: '15rem', width: '100%' }}>
+                    <source src={videorater} type="video/mp4" />
+                    Tu navegador no soporta el elemento de video.
+                </video>
                 <Card.Body>
-                    <Card.Text style={{ textAlign: 'justify' }}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos skañlsdklakdlsajdkasdkj asljdls adjlasdjl askdjlksaj asldkjasjd lasjdlkasj dlksajd l
+                    <div className="d-flex flex-wrap justify-content-center text-center">
+                        <Badge className="m-1" style={badgeStyle}>
+                            HTML
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            CSS
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            JavaScript
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            CanvasJS
+                        </Badge>
+                        {/* Agrega más etiquetas Badge según las tecnologías utilizadas */}
+                    </div>
+
+                    <Card.Text style={{ textAlign: 'justify' }} className="mt-2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos skañlsdklakdlsajdkasdkj asljdls
+                        adjlasdjl askdjlksaj asldkjasjd lasjdlkasj dlksajd l
                     </Card.Text>
                     <div className="d-flex justify-content-center">
-                        <Button variant="primary" className="me-2" href="URL_PROYECTO_1" target="_blank" rel="noopener noreferrer">
-                            Ver web
+                        <Button variant="primary" className="me-4 custom-btn" href="URL_PROYECTO_1" target="_blank" rel="noopener noreferrer">
+                            Web
                         </Button>
-                        <Button variant="secondary" href="URL_REPO_1" target="_blank" rel="noopener noreferrer">
-                            Repositorio en Github
+                        <Button variant="secondary" className="custom-btn" href="URL_REPO_1" target="_blank" rel="noopener noreferrer">
+                            Repo Github
                         </Button>
                     </div>
                 </Card.Body>
             </Card>
+
             <Card className="mb-3 mt-5 mx-auto" style={{ ...cardStyle, width: '40rem' }}>
-                <Card.Img variant="top" src={img1} className="p-3" style={{ objectFit: 'contain', maxHeight: '15rem' }} />
+                <h5 className="text-center mb-1 mt-3">Web 1 Backend</h5>
+                <video autoPlay loop className="p-3" style={{ maxHeight: '15rem', width: '100%' }}>
+                    <source src={videorick} type="video/mp4" />
+                    Tu navegador no soporta el elemento de video.
+                </video>
                 <Card.Body>
-                    <Card.Text style={{ textAlign: 'justify' }}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos skañlsdklakdlsajdkasdkj asljdls adjlasdjl askdjlksaj asldkjasjd lasjdlkasj dlksajd l
+                    <div className="d-flex flex-wrap justify-content-center text-center">
+                        <Badge className="m-1" style={badgeStyle}>
+                            HTML
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            CSS
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            JavaScript
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            ExpressJS
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            NodeJS
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            MongoDB
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            Mongoose
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            Bootstrap
+                        </Badge>
+                        {/* Agrega más etiquetas Badge según las tecnologías utilizadas */}
+                    </div>
+
+                    <Card.Text style={{ textAlign: 'justify' }} className="mt-2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos skañlsdklakdlsajdkasdkj asljdls
+                        adjlasdjl askdjlksaj asldkjasjd lasjdlkasj dlksajd l
                     </Card.Text>
                     <div className="d-flex justify-content-center">
-                        <Button variant="primary" className="me-2" href="URL_PROYECTO_1" target="_blank" rel="noopener noreferrer">
-                            Ver web
+                        <Button variant="primary" className="me-4 custom-btn" href="URL_PROYECTO_1" target="_blank" rel="noopener noreferrer">
+                            Web
                         </Button>
-                        <Button variant="secondary" href="URL_REPO_1" target="_blank" rel="noopener noreferrer">
-                            Repositorio en Github
+                        <Button variant="secondary" className="custom-btn" href="URL_REPO_1" target="_blank" rel="noopener noreferrer">
+                            Repo Github
                         </Button>
                     </div>
                 </Card.Body>
             </Card>
 
-        </div>
-    )
-}
+            <Card className="mb-3 mt-5 mx-auto" style={{ ...cardStyle, width: '40rem' }}>
+                <h5 className="text-center mb-1 mt-3">Web MERN 360</h5>
+                <video autoPlay loop className="p-3" style={{ maxHeight: '15rem', width: '100%' }}>
+                    <source src={video360} type="video/mp4" />
+                    Tu navegador no soporta el elemento de video.
+                </video>
+                <Card.Body>
+                    <div className="d-flex flex-wrap justify-content-center text-center">
+                        <Badge className="m-1" style={badgeStyle}>
+                            MongoDB
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            ExpressJS
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            ReactJS
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            NodeJS
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            HTML
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            CSS
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            JavaScript
+                        </Badge>
+                        <Badge className="m-1" style={badgeStyle}>
+                            Bootstrap
+                        </Badge>
+                        {/* Agrega más etiquetas Badge según las tecnologías utilizadas */}
+                    </div>
 
-export default Cards
+                    <Card.Text style={{ textAlign: 'justify' }} className="mt-2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos skañlsdklakdlsajdkasdkj asljdls
+                        adjlasdjl askdjlksaj asldkjasjd lasjdlkasj dlksajd l
+                    </Card.Text>
+                    <div className="d-flex justify-content-center">
+                        <Button variant="primary" className="me-4 custom-btn" href="URL_PROYECTO_1" target="_blank" rel="noopener noreferrer">
+                            Web
+                        </Button>
+                        <Button variant="secondary" className="custom-btn" href="URL_REPO_1" target="_blank" rel="noopener noreferrer">
+                            Repo Github
+                        </Button>
+                    </div>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
+
+export default Cards;
--- src/LeftColumn.jsx
+++ src/LeftColumn.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './LeftColumn.css';
+import { Github, Envelope, Linkedin } from 'react-bootstrap-icons';
 
 const LeftColumn = ({ setShowProjects, setShowSketches }) => {
     const handleProjectsClick = () => {
@@ -16,11 +17,24 @@ const LeftColumn = ({ setShowProjects, setShowSketches }) => {
         <div>
             <h1 className="name-text custom-margin-top">Gimena Motto</h1>
             <div className="menu-text">
-                <p onClick={handleProjectsClick}>__PROYECTOS</p>
-                <p onClick={handleSketchesClick}>__BOCETOS EN P5.JS</p>
+                <p onClick={handleProjectsClick} className="menu-link">__PROYECTOS WEB</p>
+                <p onClick={handleProjectsClick} className="menu-link">__APLICACIONES ESCRITORIO</p>
+                <p onClick={handleSketchesClick} className="menu-link">__BOCETOS EN P5.JS</p>
             </div>
-        </div>
+            <div className="icon-container custom-margin-top">
+                <a href="https://github.com/tu_usuario_de_GitHub" target="_blank" rel="noopener noreferrer" className="icon">
+                    <Github size={32} />
+                </a>
+                <a href="mailto:tu_correo@gmail.com" className="icon">
+                    <Envelope size={32} />
+                </a>
+                <a href="https://www.linkedin.com/in/tu_usuario_de_LinkedIn" target="_blank" rel="noopener noreferrer" className="icon">
+                    <Linkedin size={32} />
+                </a>
+            </div>
+        </div >
     );
 };
 
 export default LeftColumn;
+
